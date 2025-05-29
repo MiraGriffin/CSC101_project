b66ab7c3ed5df863a9266ff55c6fa363c9ef3d56
graysonsix <gsix@calpoly.edu>
2025-05-28 17:18:05 -0700

    Hi

---
+++ classes.py
@@ -0,0 +1,41 @@
+class Student:
+    def __init__(self, name:str, id:int, assignments:dict,
+                 overall:float, quarter:dict, gpa:float):
+        self.name = name
+        self.id = id
+        self.assignments = assignments
+        self.overall = overall
+        self.quarter = quarter
+        self.gpa = gpa
+    def __repr__(self):
+        return ("Name: {}, Student ID: {}, Grades by Assignment: {}, Overall Grade: {}, Grades by Quarter: {}, GPA: {}"
+                .format(self.name,self.id,self.assignments,self.overall,self.quarter,self.gpa))
+    def __str__(self):
+        return(("Name: {}, Student ID: {}, Grades by Assignment: {}, Overall Grade: {}, Grades by Quarter: {}, GPA: {}"
+                .format(self.name,self.id,self.assignments,self.overall,self.quarter,self.gpa)))
+    def __eq__(self, other):
+        return (self is other or
+                type(other) == Student and
+                self.name == other.name and
+                self.id == other.id and
+                self.assignments == other.assignments and
+                self.overall == other.overall and
+                self.quarter == other.quarter and
+                self.gpa == other.gpa)
+
+class Classes:
+    def __init__(self, name:str, grade:int, quarter_taken:str):
+        self.name = name
+        self.grade = grade
+        self.quarter_taken = quarter_taken
+    def __repr__(self):
+        return "Class: {}, Grade: {}, Quarter Taken: {}".format(self.name,self.grade,self.quarter_taken)
+    def __str__(self):
+        return "Class: {}, Grade: {}, Quarter Taken: {}".format(self.name, self.grade, self.quarter_taken)
+
+
+
+
+
+
+
